467b2bed43b84301838394d329b86d1eb3a77835
rencho13 <stephone13@gmail.com>
2019-09-17 17:50:25 +0200

    Menu dans IHM, nettoyage du Main().

--- src/fr/wcs/nfs/dicoutils/DicoIhm.java
+++ src/fr/wcs/nfs/dicoutils/DicoIhm.java
@@ -1,14 +1,35 @@
-/**
- * 
- *
- */
 package fr.wcs.nfs.dicoutils;
 
-/**
- * @author franck
- *
- */
+import java.util.Scanner;
+
 public class DicoIhm {
+	
+	private String messageEnvoye="";
+	Scanner saisieUtilisateur = new Scanner(System.in);
+	private String choixMenu;
+	private String texte;
+	private boolean isRunning;
+	
+		
+	public String getChoixMenu() {
+		return choixMenu;
+	}
+
+	public void setChoixMenu(String choixMenu) {
+		this.choixMenu = choixMenu;
+	}
+
+	public String getTexte() {
+		return texte;
+	}
+
+	public void setTexte(String texte) {
+		this.texte = texte;
+	}
+
+	public void setMessageEnvoye(String messageEnvoye) {
+		this.messageEnvoye = messageEnvoye;
+	}
 
 	public DicoIhm() {
 		
@@ -16,7 +37,6 @@ public class DicoIhm {
 	
 	public void affichageMenu() {
 		String menu;
-		
 		menu =  "************************\n"+
 				"************************\n"+
 				"*     DICO UTILS       *\n"+
@@ -29,7 +49,50 @@ public class DicoIhm {
 				"*     5- CHOIX         *\n"+
 				"*     0- Quitter       *\n"+
 				"************************";
-		
-		System.out.println(menu);
+		System.out.println(menu);	
 	}
+	
+	public void affichageMessage(String message) {
+		messageEnvoye="";
+		messageEnvoye += message;
+		System.out.println(messageEnvoye);
+	}
+	public String choixUtilisateur() {
+		
+		choixMenu = saisieUtilisateur.nextLine();
+		
+		return choixMenu;
+	}
+	
+	public void start() {
+		isRunning = true;
+		
+		do 
+		{
+			affichageMenu();
+			affichageMessage("Entrer votre choix: ");
+			setChoixMenu(choixUtilisateur());
+			
+			//Faire un switch case
+			
+			
+//	        if(this.getChoixMenu()!="0" || this.getChoixMenu()!="1" ||
+//					this.getChoixMenu()!="2" ||this.getChoixMenu()!="3" ||this.getChoixMenu()!="4" || this.getChoixMenu()!="5") {
+//	        	
+//	        	affichageMessage("Nombre compris entre 0 et 5");
+//	        }else {
+//	        	
+//	        	affichageMessage("Vous avez choisi le menu: "+getChoixMenu() + "\n");
+//	        	affichageMessage("Veuillez saisir votre recherche: ");
+//	        	setTexte(choixUtilisateur());
+//	        	affichageMessage("vous avez saisi: " + getTexte());
+//	        	isRunning=false;
+//	        }
+	
+		} while(isRunning);
+		
+	}
+
+
+
 }
--- src/fr/wcs/nfs/dicoutils/DicoUtils.java
+++ src/fr/wcs/nfs/dicoutils/DicoUtils.java
@@ -1,51 +1,18 @@
 package fr.wcs.nfs.dicoutils;
 
-import java.util.Scanner;
 
 public class DicoUtils {
 
+	
 	public static void main(String[] args) {
 		
-		boolean isRunning=true;
-		Scanner saisieUtlisateur = new Scanner(System.in);	
 		DicoIhm dico = new DicoIhm();
-		int choixMenu;
-		String saisiRecherche;
-		//Dicoloader fichier= new Dicoloader();
-	
-		while (isRunning) 
-		{
-			
-			dico.affichageMenu();
-			
-			
-			System.out.print("Entrer votre choix: ");  
-	          choixMenu = saisieUtlisateur.nextInt(); 
-	          
-			if (choixMenu<0 || choixMenu>6) {
-				isRunning=true;
-				System.out.println("Votre choix doit etre entre 0 et 5 ");
-			}else {
-				
-				choixUtilisateur(choixMenu);
-				isRunning=false;
-			}
-			
-			
-		}
+		
+		dico.start();
+		
 		
 	}
 	
-		
-		
 	
-	
-
-	private static void choixUtilisateur(int choix) {
-		
-		System.out.println("Vous avez choix l'option"+ choix);
-		
-	}
-
 }
 
