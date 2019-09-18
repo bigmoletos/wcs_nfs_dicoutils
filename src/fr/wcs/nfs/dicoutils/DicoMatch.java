46ba4d1fc642e0e8dbb1552678fa63e1ebca0492
Nin1963 <ninayamina1963@gmail.com>
2019-09-18 09:34:37 +0200

    fonctions DicoMatch 4 sur 5

--- src/fr/wcs/nfs/dicoutils/DicoMatch.java
+++ src/fr/wcs/nfs/dicoutils/DicoMatch.java
@@ -9,5 +9,79 @@ package fr.wcs.nfs.dicoutils;
  *
  */
 public class DicoMatch {
+	
+	static Boolean match = false;
+	
+	public boolean wordIsPresent (String[] mots, String mot) {
+		
+		for (int i = 0; i < mots.length; i ++) {
+			if (mots[i] == mot) {
+				match = true;
+				System.out.println("Ok, le mot est dans le dico!");}
+			
+		}
+		return match;
+	}
+	
+		
+		
+		public static boolean startBy (String[] mots, String str) {
 
-}
+			
+			for(int i = 0; i < mots.length; i ++) {
+				
+				if (mots[i].startsWith(str)) {
+					match=true;
+				}
+				
+				
+			}
+			return match;
+		}
+		
+		public static boolean endBy (String[] mots, String str) {
+
+			for(int i = 0; i < mots.length; i ++) {
+				if (mots[i].endsWith(str)) {
+					match=true;
+				}
+			}
+			return match;
+		}
+		
+		public static void main(String[] args) {
+			
+			String str = "123";
+			String[] mots = {
+					"abaissable", 
+					"abaissables", 
+					"abaissai", 
+					"abaissaient",
+					"hébergeriez",
+					"hébergerions",
+					"hébergerons",
+					"second",
+					"seconda",
+					"secondai"
+					
+			
+			};
+		
+			System.out.println(content(mots, str));
+		}
+			
+			public static boolean content (String[] mots, String mot) {
+				
+				for(int i = 0; i < mots.length; i ++) {
+					if (mots[i].contains(mot)) {
+						match=true;
+					}
+				}
+				return match;
+				}
+			
+			
+		}
+	
+		
+
