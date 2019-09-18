0ea74bdf50608e154ab2b37945899498bd15af73
Nin1963 <ninayamina1963@gmail.com>
2019-09-18 13:06:35 +0200

    Ajout de commentaires

--- src/fr/wcs/nfs/dicoutils/DicoMatch.java
+++ src/fr/wcs/nfs/dicoutils/DicoMatch.java
@@ -3,6 +3,7 @@
  *
  */
 package fr.wcs.nfs.dicoutils;
+import java.util.regex.*;
 
 /**
  * @author franck
@@ -17,7 +18,8 @@ public class DicoMatch {
 		for (int i = 0; i < mots.length; i ++) {
 			if (mots[i] == mot) {
 				match = true;
-				System.out.println("Ok, le mot est dans le dico!");}
+				//System.out.println("Ok, le mot est dans le dico!");
+				}
 			
 		}
 		return match;
@@ -25,63 +27,65 @@ public class DicoMatch {
 	
 		
 		
-		public static boolean startBy (String[] mots, String str) {
+	public static boolean startBy (String[] mots, String str) {
 
+		
+		for(int i = 0; i < mots.length; i ++) {
 			
-			for(int i = 0; i < mots.length; i ++) {
-				
-				if (mots[i].startsWith(str)) {
-					match=true;
-				}
-				
-				
+			if (mots[i].startsWith(str)) {
+				match=true;
 			}
-			return match;
-		}
-		
-		public static boolean endBy (String[] mots, String str) {
-
-			for(int i = 0; i < mots.length; i ++) {
-				if (mots[i].endsWith(str)) {
-					match=true;
-				}
-			}
-			return match;
-		}
-		
-		public static void main(String[] args) {
-			
-			String str = "123";
-			String[] mots = {
-					"abaissable", 
-					"abaissables", 
-					"abaissai", 
-					"abaissaient",
-					"hébergeriez",
-					"hébergerions",
-					"hébergerons",
-					"second",
-					"seconda",
-					"secondai"
-					
-			
-			};
-		
-			System.out.println(content(mots, str));
-		}
-			
-			public static boolean content (String[] mots, String mot) {
-				
-				for(int i = 0; i < mots.length; i ++) {
-					if (mots[i].contains(mot)) {
-						match=true;
-					}
-				}
-				return match;
-				}
 			
 			
 		}
+		return match;
+	}
 	
+	public static boolean endBy (String[] mots, String str) {
+
+		for(int i = 0; i < mots.length; i ++) {
+			if (mots[i].endsWith(str)) {
+				match=true;
+			}
+		}
+		return match;
+	}
+	
+	
+		
+		public static boolean content (String[] mots, String mot) {
+			
+			for(int i = 0; i < mots.length; i ++) {
+				if (mots[i].contains(mot)) {
+					match=true;
+				}
+			}
+			return match;
+			}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		String str = "ab";
+		String[] mots = {
+				"abaissable", 
+				"abaissables", 
+				"abaissai", 
+				"abaissaient",
+				"hébergeriez",
+				"hébergerions",
+				"hébergerons",
+				"second",
+				"seconda",
+				"secondai"
+				
+		
+		};
+	
+		System.out.println(content(mots, str));
+	}
+			
 		
 
